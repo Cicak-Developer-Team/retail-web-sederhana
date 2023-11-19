36ca4ad1b9a835fd2af1d334d199d4d3e97bac67
TheZaww <testizaw1@gmail.com>
2023-11-18 19:21:14 -0800

    Hudanggggggggggggggggggggggg ! dashboad update

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class Dashboard extends Controller
 {
     function index(){
-        return view("dashboard.index");
+        $categories = Category::all();
+        return view("dashboard.index",[
+            "categories" => $categories
+        ]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', "Login Page")
 @section('content')
+<style>
+    label {
+        text-transform: capitalize;
+    }
+</style>
 <div class="container">
     <div class="row">
         <div class="col-md-12 mb-3 bg-light rounded shadow" style="justify-content: space-evenly;">
@@ -15,6 +20,41 @@
                     <a href="/logout" class="btn btn-danger">
                         Logout
                     </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3 p-2 rounded shadow">
+                    <form class="">
+                        <h4>Tambah Prduk</h4>
+                        <div class="mb-3">
+                            <label class="form-label">Category ID</label>
+                            <select name="category_id" class="form-select">
+                                <option value="">---</option>
+                                @foreach( $categories as $row )
+                                    <option value="{{ $row->id }}">{{$row->id . " - " . $row->nama}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">nama</label>
+                            <input type="text" name="nama" class="form-control" placeholder="nama">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">quantity</label>
+                            <input type="number" name="quantity" class="form-control" placeholder="quantity">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">harga</label>
+                            <input type="text" name="harga" class="form-control" placeholder="harga">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">deskripsi</label>
+                            <textarea name="deskipsi" class="form-control"></textarea>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
